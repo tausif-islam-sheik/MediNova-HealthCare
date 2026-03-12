0460c454634f166daad9a75411d26a4b97a6ef90
Tausif Islam Sheik <mr.tausif904@gmail.com>
2026-03-13 05:30:18 +0600

    Added Not Found Page , Error Page, Loading Pages

---
+++ src/app/(dashboardLayout)/admin/dashboard/doctor-specialities-management/loading.tsx
@@ -0,0 +1,4 @@
+export default function AdminsDoctorSpecialitiesManagementLoading() {
+  // Or a custom loading skeleton component
+  return <p>Loading Admins Doctor Specialities Management...</p>;
+}
---
+++ src/app/(dashboardLayout)/admin/dashboard/specialities-management/loading.tsx
@@ -0,0 +1,4 @@
+export default function AdminsSpecialitiesManagementLoading() {
+  // Or a custom loading skeleton component
+  return <p>Loading Admins Specialities Management...</p>;
+}
