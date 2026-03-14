3b061a9a5275b82214620dc6bb99d9f0005d4735
Tausif Islam Sheik <mr.tausif904@gmail.com>
2026-03-14 23:54:16 +0600

    feat: add TanStack Query prefetch & dehydrate for Next.js server-side data fetching

--- src/app/(commonLayout)/consultation/page.tsx
+++ src/app/(commonLayout)/consultation/page.tsx
@@ -1,5 +1,25 @@
-const ConsultationPage = () => {
-  return <div>ConsultationPage</div>;
+import DoctorsList from "@/components/modules/Consultation/DoctorsList";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import { getDoctors } from "./_actions";
+
+const ConsultationPage = async () => {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["doctors"],
+    queryFn: getDoctors,
+  });
+  return (
+    // Neat! Serialization is now as easy as passing props.
+    // HydrationBoundary is a Client Component, so hydration will happen there.
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <DoctorsList />
+    </HydrationBoundary>
+  );
 };
 
 export default ConsultationPage;
