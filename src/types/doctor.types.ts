9ffde2c7d3bdb17299cf0100afee726813d4128b
Tausif Islam Sheik <mr.tausif904@gmail.com>
2026-03-26 15:59:54 +0600

    feat: Create Data Table Columns With Actions

--- src/types/doctor.types.ts
+++ src/types/doctor.types.ts
@@ -1,7 +1,41 @@
+enum Gender {
+  MALE = "MALE",
+  FEMALE = "FEMALE",
+  OTHER = "OTHER",
+}
+
+export enum UserStatus {
+  ACTIVE = "ACTIVE",
+  BLOCKED = "BLOCKED",
+  DELETED = "DELETED",
+}
+
 export interface IDoctor {
   id: number;
   name: string;
-  specialization: string;
-  experience: number;
-  rating: number;
+  email: string;
+  profilePhoto?: string;
+  contactNumber?: string;
+  address?: string;
+  registrationNumber: string;
+  experience?: number;
+  gender: Gender;
+  appointmentFee: number;
+  qualification: string;
+  currentWorkingPlace: string;
+  designation: string;
+  averageRating: number;
+  createdAt: Date;
+  user: {
+    status: UserStatus;
+  };
+  specialities: Array<{
+    specialityId: string;
+    doctorId: string;
+    speciality: {
+      id: string;
+      title: string;
+      icon: string;
+    };
+  }>;
 }
